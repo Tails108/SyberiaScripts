731e6eede9651f9638fead860ddb9a7cdddbc2fd
Tails108 <125358631+Tails108@users.noreply.github.com>
2023-08-29 14:14:13 +0100

    initial commit

---
+++ src/scripts/4_World/Classes/Recipes/FillGasMask_Filter.c
@@ -0,0 +1,9 @@
+modded class FillGasMask_Filter extends RecipeBase	
+{	
+	override void Init()
+	{
+		super.Init();
+		InsertIngredient(0,"TabletsCharcoal");//you can insert multiple ingredients this way		
+	}
+
+};
